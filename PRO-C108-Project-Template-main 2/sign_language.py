59b2679f3225c5c890b3bd57a74769702cf125f8
Charvisaxena01 <74494231+Charvisaxena01@users.noreply.github.com>
2023-01-12 12:46:20 +0530

    Update sign_language.py

--- PRO-C108-Project-Template-main 2/sign_language.py
+++ PRO-C108-Project-Template-main 2/sign_language.py
@@ -9,6 +9,9 @@ cap = cv2.VideoCapture(0)
 finger_tips =[8, 12, 16, 20]
 thumb_tip= 4
 
+finger_fold_status=[]
+
+
 while True:
     ret,img = cap.read()
     img = cv2.flip(img, 1)
@@ -19,13 +22,16 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmark in results.multi_hand_landmarks:
             #accessing the landmarks by their position
+            lm_list=[]
+            for id ,lm in enumerate(hand_landmark.landmark):
+                lm_list.append(lm)
 
             for tip in finger_tips:
-                x,y = int(lm_list[tip].x*w),int(llm_list[tip].y*h)
+                x,y = int(lm_list[tip].x*w),int(lm_list[tip].y*h)
                 cv2.circle(img,(x,y),15,(255,0,0),cv2.FILLED)
 
                 if lm_list[tip].x< lm_list[tip-2].x:
-                    cv2.circle(img,(x,y),15,(0,255,0),cv2.FILED)
+                    cv2.circle(img,(x,y),15,(0,255,0),cv2.FILLED)
                     finger_fold_status.append(True)
                 else:
                     finger_fold_status.append(False)
@@ -33,21 +39,18 @@ while True:
                 if all(finger_fold_status):
                     if lm_list[thumb_tip].y< lm_list[thumb_tip-2].y:
                         print("like")
-                        cv2.puText(img,"like", (20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,),3)
+                        cv2.putText(img,"like", (20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,),3)
 
                         if lm_list[thumb_tip].y>lm_list[thumb_tip-2].y:
                             print("dislike")
-                            cv2.puText(img,"dislike", (20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,),3)
+                            cv2.putText(img,"dislike", (20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,),3)
 
 
     
-            lm_list=[]
-            for id ,lm in enumerate(hand_landmark.landmark):
-                lm_list.append(lm)
+
 
              #Code goes here
-            finger_fold_status=[]
-
+            
                            
 
                 
